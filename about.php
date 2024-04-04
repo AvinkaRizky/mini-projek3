29fe0311520b6b8f82a0d4c384528bde91296dd4
Avinka Rizky <avinkarizky242@gmail.com>
2024-04-04 20:49:12 +0800

    first commit

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Dreams Cake merupakan toko yang sudah banyak dikenal oleh masyarakat dikarenakan rasanya yang enak dan bergizi.</p>
+<p>
+Dreams Cake merupakan toko yang menjual aneka kue, yang digemari oleh banyak orang.</p>
+</div>
+
+ <?php 
+	include 'footer.php';
+ ?>
